6fbf896a52e0f9c35ed2aba0a55b952bcd3eef98
MatiasJoaquinAlfonso <joaquinmatias332@gmail.com>
2025-10-23 14:03:53 -0300

    Cambios en las dependencias e incio de implementacion para el apptheme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,21 +2,34 @@ import 'package:flutter/material.dart';
 
 const colorList = <Color>[
   Colors.blue,
+  Colors.deepPurple,
   Colors.red,
 ];
 
 class AppTheme {
 
   final int selectedColor;
+  final bool isDarkMode;
 
   AppTheme({
+    this.isDarkMode = false,
     this.selectedColor = 0,
   }): assert(selectedColor >= 0, 'El color seleccionado no es valido.');
 
   ThemeData getTheme() => ThemeData(
     useMaterial3: true,
+    brightness: isDarkMode ?  Brightness.dark : Brightness.light,
     colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: AppBarTheme(centerTitle: false,)
     // useSystemColors: 
   ); 
+
+  AppTheme copyWith({
+    int? selectedColor,
+    bool? isDarkMode,
+  }) => AppTheme(
+    selectedColor: selectedColor?? this.selectedColor,
+    isDarkMode: isDarkMode?? this.isDarkMode,
+  );
 
 }
--- lib/features/screens/home_page.dart
+++ lib/features/screens/home_page.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:todo_app/config/providers/theme_provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
+
+
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    // final isDarkMode = ref.watch( themeNotifierProvider).isDarkMode;
+    
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('TO-DO APP'),
-        backgroundColor: Colors.blue.shade600,
+        // backgroundColor: Colors.blue.shade600,
         actionsPadding: EdgeInsets.symmetric(horizontal: 10),
         actions: [
-          Icon(Icons.dark_mode_outlined)
+          IconButton(
+            onPressed: () {
+              
+            },
+            icon: Icon(Icons.dark_mode_outlined),
+          )
         ],
       ),
-      backgroundColor: Colors.white70,
+      // backgroundColor: Colors.white70,
       body: Center(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-          decoration: BoxDecoration(
-            color: Colors.redAccent,
-            borderRadius: BorderRadius.circular(20),
-          ),
-        )
+        // child: 
       ),
 
     );
