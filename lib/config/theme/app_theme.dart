d8a149c23f8c293f5ccbc0e8fc3912acff69f218
MatiasJoaquinAlfonso <joaquinmatias332@gmail.com>
2026-02-02 10:36:29 -0300

    Cambios en el app theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -16,13 +16,45 @@ class AppTheme {
     this.selectedColor = 0,
   }): assert(selectedColor >= 0, 'El color seleccionado no es valido.');
 
-  ThemeData getTheme() => ThemeData(
-    useMaterial3: true,
-    brightness: isDarkMode ?  Brightness.dark : Brightness.light,
-    colorSchemeSeed: colorList[selectedColor],
-    appBarTheme: AppBarTheme(centerTitle: false,)
-    // useSystemColors: 
-  ); 
+  ThemeData getTheme() {
+    
+    final colorScheme = ColorScheme.fromSeed(
+        seedColor: colorList[selectedColor],
+        brightness: isDarkMode ?  Brightness.dark : Brightness.light,
+      );
+
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: colorScheme,
+
+      // scaffoldBackgroundColor: isDarkMode ? null : Colors.white60,
+
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        // backgroundColor: isDarkMode? null : Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: colorScheme.primary),
+        titleTextStyle: TextStyle(
+          color: isDarkMode ? Colors.white : Colors.black87,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      cardTheme: CardThemeData(
+        color: isDarkMode ? null : Colors.white30,
+        elevation: 2, 
+        shadowColor: Colors.black12, 
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      ),
+
+      textTheme: TextTheme(
+        titleLarge: TextStyle(fontWeight: FontWeight.bold),
+      )
+
+    );
+
+  }
 
   AppTheme copyWith({
     int? selectedColor,
